7148a4a8da5c77674c1b7a567ae2d4d9ba5a104b
emk1online <emk1online@gmail.com>
2023-02-14 14:57:54 +0500

    Добавьте файлы проекта.

---
+++ AutoService/Models/PartialClasses/ServicePhotoPartial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.Models
+{
+    public partial class ServicePhoto
+    {
+        public string MainImage { get
+            {
+
+                return "..\\..\\Assets\\img\\"+PhotoPath;
+                    }
+        }
+    }
+}
